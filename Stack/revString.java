3ba634dbfe8dc9f2de14f6463254b2886522138f
Sandeepgupta078 <sandeepmgs078@gmail.com>
2024-08-01 10:53:39 +0530

    problems on stack

---
+++ Stack/revString.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.Stack;
+
+public class revString {
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int index = 0;
+
+        while(index<str.length()){
+            s.push(str.charAt(index));
+            index++;
+        }
+        StringBuilder result = new StringBuilder();
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String str = "Sandeep";
+        String result = reverseString(str);
+        System.out.println(result );
+    }
+}
