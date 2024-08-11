0c6c923717884a44d9bbed63cdebac6dc0865321
Sandeepgupta078 <sandeepmgs078@gmail.com>
2024-08-11 23:41:56 +0530

    pronlems solved

--- Stack/validParanthesis.java
+++ Stack/validParanthesis.java
@@ -28,8 +28,36 @@ public class validParanthesis {  //o(n)
             return false;
         }
     }
+
+    // duplicate parenthesis
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        // closing
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            
+            if(ch == ')'){
+                int count = 0;
+                while(s.pop() != '('){
+                    count++;
+                }
+                if(count < 1){
+                    return true;
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        String str = "({[({})]})]";
-        System.out.println(isValid(str));
+        // String str = "({[({})]})]";
+        // System.out.println(isValid(str));
+
+        String str = "(((a+b) + (c+d)))";
+        System.out.println(isDuplicate(str));
+
     }
 }
